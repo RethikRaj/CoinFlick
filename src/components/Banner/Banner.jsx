cd310cf7cf9581554f3a2a6ec6147d5db9f46d5c
RethikRaj <rethikrajrr@gmail.com>
2025-04-13 10:28:48 +0530

    edited navbar and banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,14 @@ import BannerImage from '../../assets/images/banner2.jpeg'
 
 function Banner(){
     return (
-        <div className='w-full '>
-            <img src={BannerImage} alt='Banner' className='w-full h-full'/>
+        <div className='relative w-full'>
+            <img src={BannerImage} alt='Banner' className='w-full'/>
+
+            <div className='absolute top-25 w-full flex flex-col justify-center items-center gap-4'>
+                <h1 className='text-4xl font-bold text-white text-center'>CoinFlick: Track the Crypto Market with Precision</h1>
+                <h2 className='text-md font-bold text-white text-center'>Real-Time Insights. Smarter Crypto Moves.</h2>
+            </div>
+
         </div>
     )
 }
